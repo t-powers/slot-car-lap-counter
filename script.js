44dc9ff5e1954fd0e4ad3731fcd4bbd2653dbecd
t-powers <127955989+t-powers@users.noreply.github.com>
2023-10-17 07:12:43 -0400

    added reset btn and connection

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let count = document.getElementById("counter");
+
 let lap = 0;
+console.log(lapReset);
 
 function incLap() {
   lap += 1;
   count.innerText = lap;
+}
+
+function reset() {
+  count.innerText = 0;
+  lap = 0;
 }
