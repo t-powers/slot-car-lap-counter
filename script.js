df6149b9b27f2908ed70479b254d29ee3d5a2aa3
t-powers <127955989+t-powers@users.noreply.github.com>
2023-10-21 11:33:41 -0400

    added comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let count = document.getElementById("counter");
 let lap = 3;
 
+//lap btn activates countdown
 function incLap() {
   if (lap > 0) {
     lap -= 1;
@@ -8,18 +9,20 @@ function incLap() {
   }
 }
 
+//activated default lap count
 function reset() {
   count.innerText = 3;
   lap = 3;
 }
 
+//decreases total count
 function minLaps() {
   if (lap > 1) {
     lap -= 1;
     count.innerText = lap;
   }
 }
-
+//increases total count
 function addLaps() {
   lap += 1;
   count.innerText = lap;
